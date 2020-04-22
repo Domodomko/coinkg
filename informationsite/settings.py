991de5d5b05b5b32b1a2c54952fd41454775569b
Domodom <viablochko@gmail.com>
2020-04-22 16:06:34 +0600

    s

--- informationsite/settings.py
+++ informationsite/settings.py
@@ -154,6 +154,7 @@ TIME_FORMAT = 'H:i'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_TMP = os.path.join(BASE_DIR, 'static')
