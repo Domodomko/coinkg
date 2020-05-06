b9220b3d128d7ef2656ed14d08178b018ca78e91
Domodom <viablochko@gmail.com>
2020-05-06 16:24:00 +0600

    kek

--- main/serializers.py
+++ main/serializers.py
@@ -1,21 +1,44 @@
 from rest_framework import serializers
-
+from django.conf import settings
+from modeltranslation.manager import get_translatable_fields_for_model
 from .models import *
 
 
-class NewsSerializer(serializers.ModelSerializer):
+class TranslatedModelSerializerMixin(serializers.ModelSerializer):
+    def get_field_names(self, declared_fields, info):
+        fields = super().get_field_names(declared_fields, info)
+        trans_fields = get_translatable_fields_for_model(self.Meta.model)
+        all_fields = []
+
+        requested_langs = []
+        if 'request' in self.context:
+            lang_param = self.context['request'].query_params.get('lang', None)
+            requested_langs = lang_param.split(',') if lang_param else []
+
+        for f in fields:
+            if f not in trans_fields:
+                all_fields.append(f)
+            else:
+                for l in settings.LANGUAGES:
+                    if not requested_langs or l[0] in requested_langs:
+                        all_fields.append("{}_{}".format(f, l[0]))
+
+        return all_fields
+
+
+class NewsSerializer(TranslatedModelSerializerMixin, serializers.ModelSerializer):
     class Meta:
         model = News
         fields = ('id', 'title', 'content', 'publish', 'views', 'image')
 
 
-class SuccessStorySerializer(serializers.ModelSerializer):
+class SuccessStorySerializer(TranslatedModelSerializerMixin, serializers.ModelSerializer):
     class Meta:
         model = SuccessStory
         fields = ('title', 'content')
 
 
-class AboutUsSerializer(serializers.ModelSerializer):
+class AboutUsSerializer(TranslatedModelSerializerMixin, serializers.ModelSerializer):
     class Meta:
         model = AboutUs
         fields = ('content',)
@@ -34,14 +57,14 @@ class FeedbackSerializer(serializers.ModelSerializer):
 
 
 class CreditSerializer(serializers.ModelSerializer):
-    user = serializers.ReadOnlyField(source='user.email')
+    # user = ProfileSerializer(many=True)
 
     class Meta:
         model = Credit
         fields = ('id', 'product', 'sum', 'time', 'passport', 'publish', 'currency', 'other_credits', 'user')
 
 
-class CreditsInfoSerializer (serializers.ModelSerializer):
+class CreditsInfoSerializer(TranslatedModelSerializerMixin, serializers.ModelSerializer):
     class Meta:
         model = CreditsInfo
         fields = ('id', 'title', 'content', 'image')
--- main/translation.py
+++ main/translation.py
@@ -6,21 +6,24 @@ from .models import *
 @register(News)
 class NewsTranslationOption(TranslationOptions):
     fields = ('title', 'content')
-    required_languages = ('ru',)
+    required_languages = ('ru', 'kg', 'en')
 
 
 @register(SuccessStory)
 class SuccessStoryTranslationOption(TranslationOptions):
     fields = ('title', 'content')
+    required_languages = ('ru', 'kg', 'en')
 
 
 @register(AboutUs)
 class AboutUsTranslationOption(TranslationOptions):
     fields = ('content',)
+    required_languages = ('ru', 'kg', 'en')
 
 
 @register(CreditsInfo)
 class CreditsInfoTranslationOption(TranslationOptions):
     fields = ('title', 'content',)
+    required_languages = ('ru', 'kg', 'en')
 
 
