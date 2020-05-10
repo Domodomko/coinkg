91928f9631ff239e4f77a7b4b9940cce62c94e54
Domodom <viablochko@gmail.com>
2020-05-10 18:47:53 +0600

    Update admin.py

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
 from .models import User
 
-admin.site.register(User)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('name', 'surname', 'email', 'is_active', 'is_staff')
+    search_fields = ('email', 'name', 'surname',)
+    readonly_fields = ()
+
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = (
+        'name', 'surname', 'patronymic', 'email', 'phone', 'address', 'birthday', 'job', 'is_active', 'is_staff')
+
+
+admin.register(User, UserAdmin)
